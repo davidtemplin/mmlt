8a59117f1e903e21a4bc10520a6f26fe1c36e415
David Templin <dtemplin@tilled.com>
2024-02-27 08:26:19 -0600

    add basic command-line argument parsing

--- src/config.rs
+++ src/config.rs
@@ -4,12 +4,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn parse(_args: Vec<String>) -> Result<Config, String> {
-        // TODO!!
+    pub fn parse(args: Vec<String>) -> Result<Config, String> {
+        let mut scene_path: Option<String> = None;
+        let mut image_path: Option<String> = None;
+
+        for chunk in args.chunks(2) {
+            let flag = &chunk[1];
+            let value = &chunk[2];
+
+            match flag.as_str() {
+                "--scene" => scene_path.replace(value.clone()),
+                "--image" => image_path.replace(value.clone()),
+                _ => return Err(format!("unknown flag: {}", value)),
+            };
+        }
+
         let config = Config {
-            scene_path: String::from("/Users/david/Desktop/mmlt/scenes/scene-1.yml"),
-            image_path: String::from("/Users/david/Desktop/image.pfm"),
+            scene_path: scene_path.ok_or("--scene is required")?,
+            image_path: image_path.ok_or("--image is required")?,
         };
+
         Ok(config)
     }
 }
