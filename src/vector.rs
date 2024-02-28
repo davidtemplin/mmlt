0e3740de998c2bea4e12ae125554094fa81fed99
David Templin <dtemplin@tilled.com>
2024-02-28 14:48:07 -0600

    add tests for vector

--- src/vector.rs
+++ src/vector.rs
@@ -150,10 +150,108 @@ pub struct VectorConfig {
 mod tests {
     use crate::vector::Vector;
 
+    use super::VectorConfig;
+
+    #[test]
+    fn test_configure() {
+        let config = VectorConfig {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+        let v = Vector::configure(&config);
+        assert_eq!(v, Vector::new(1.0, 2.0, 3.0));
+    }
+
+    #[test]
+    fn test_new() {
+        let v = Vector::new(1.0, 2.0, 3.0);
+        assert_eq!(v.x, 1.0);
+        assert_eq!(v.y, 2.0);
+        assert_eq!(v.z, 3.0);
+    }
+
     #[test]
     fn test_dot() {
         let v1 = Vector::new(1.0, 2.0, 3.0);
         let v2 = Vector::new(2.0, 3.0, 4.0);
         assert_eq!(v1.dot(v2), 20.0);
+    }
+
+    #[test]
+    fn test_norm() {
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        let l1 = f64::sqrt(14.0);
+        assert_eq!(v1.norm(), Vector::new(1.0 / l1, 2.0 / l1, 3.0 / l1));
+        let v2 = Vector::new(0.0, 0.0, 2.0);
+        assert_eq!(v2.norm(), Vector::new(0.0, 0.0, 1.0));
+        let v3 = Vector::new(0.0, 0.0, 0.0);
+        assert_eq!(v3.norm(), v3);
+    }
+
+    #[test]
+    fn test_len() {
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        assert_eq!(v1.len(), f64::sqrt(14.0));
+    }
+
+    #[test]
+    fn test_cross() {
+        let v1 = Vector::new(0.0, 0.0, 1.0);
+        let v2 = Vector::new(1.0, 0.0, 0.0);
+        assert_eq!(v1.cross(v2), Vector::new(0.0, 1.0, 0.0));
+        assert_eq!(v2.cross(v1), Vector::new(0.0, -1.0, 0.0));
+        let v3 = Vector::new(1.0, 2.0, -3.0);
+        let v4 = Vector::new(0.0, -4.0, 3.0);
+        assert_eq!(v3.cross(v4), -v4.cross(v3));
+    }
+
+    #[test]
+    fn test_is_zero() {
+        let v1 = Vector::new(1e-8, 0.0, 0.0);
+        assert!(!v1.is_zero());
+        let v2 = Vector::new(0.0, 0.0, 0.0);
+        assert!(v2.is_zero());
+    }
+
+    #[test]
+    fn test_add() {
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        let v2 = Vector::new(2.0, 3.0, 4.0);
+        assert_eq!(v1 + v2, Vector::new(3.0, 5.0, 7.0));
+    }
+
+    #[test]
+    fn test_sub() {
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        let v2 = Vector::new(2.0, 3.0, 4.0);
+        assert_eq!(v1 - v2, Vector::new(-1.0, -1.0, -1.0));
+    }
+
+    #[test]
+    fn test_mul() {
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        assert_eq!(v1 * 2.0, Vector::new(2.0, 4.0, 6.0));
+        assert_eq!(-2.0 * v1, Vector::new(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn test_div() {
+        let v1 = Vector::new(1.0, 2.0, -4.0);
+        assert_eq!(v1 / 2.0, Vector::new(0.5, 1.0, -2.0));
+    }
+
+    #[test]
+    fn test_neg() {
+        let v1 = Vector::new(1.0, -2.0, 3.0);
+        assert_eq!(-v1, Vector::new(-1.0, 2.0, -3.0));
+    }
+
+    #[test]
+    fn test_eq() {
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        let v2 = Vector::new(2.0, 3.0, 4.0);
+        assert!(v1 == v1);
+        assert!(v1 != v2);
     }
 }
