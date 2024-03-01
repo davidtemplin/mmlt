3d997a2163d51165a134b3d5ff50a74e2d0f6b7f
David Templin <dtemplin@tilled.com>
2024-03-01 10:51:46 -0600

    add tests

--- src/geometry.rs
+++ src/geometry.rs
@@ -25,3 +25,40 @@ impl ApproxEq for Geometry {
             && self.direction.approx_eq(other.direction, tolerance)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Geometry;
+    use crate::{
+        approx::ApproxEq,
+        vector::{Point, Vector},
+    };
+
+    #[test]
+    fn test_geometry_eq() {
+        let g1 = Geometry {
+            point: Point::new(1.0, 1.0, 1.0),
+            normal: Vector::new(1.0, 0.0, 0.0),
+            direction: Vector::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(g1, g1);
+    }
+
+    #[test]
+    fn test_geometry_approx_eq() {
+        let g1 = Geometry {
+            point: Point::new(1.0, 1.0, 1.0),
+            normal: Vector::new(1.0, 0.0, 0.0),
+            direction: Vector::new(1.0, 1.0, 1.0),
+        };
+
+        let g2 = Geometry {
+            point: g1.point + Point::new(1e-9, 1e-9, 1e-9),
+            normal: g1.normal + Vector::new(1e-9, 1e-9, 1e-9),
+            direction: g1.direction + Vector::new(1e-9, 1e-9, 1e-9),
+        };
+
+        assert!(g1.approx_eq(g2, 1e-8));
+    }
+}
