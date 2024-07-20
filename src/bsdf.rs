7cbac062c210e90856479d3921f5335c395eaebc
David Templin <dtemplin@tilled.com>
2024-07-20 11:31:10 -0500

    tweaks

--- src/bsdf.rs
+++ src/bsdf.rs
@@ -238,6 +238,7 @@ impl Bxdf for DielectricBxdf {
     ) -> Option<Vector3> {
         // TODO: ensure paths are created correctly
         // TODO: disable reflection when internal to object; use flags?
+        // TODO: check PDF calculation and application in path.rs
         let r = self.sampling_pdf(wx, path_type)?;
         if sampler.sample(0.0..1.0) < r {
             Some(util::reflect(wx, self.normal))
